005d39719155713314d16c6c91208c8af285bcbe
Paras Jindal <pajindal@microsoft.com>
2018-09-09 20:57:03 +0530

    spaces added

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var time = function() {
     var secondAngle = -offset + second*6;
     var minuteAngle = -offset + minute*6;
     var hourAngle = -offset + hour*30;
-    document.getElementById("second").style.transform = "rotate("+secondAngle+"deg)";
-    document.getElementById("minute").style.transform = "rotate("+minuteAngle+"deg)";
-    document.getElementById("hour").style.transform = "rotate("+hourAngle+"deg)";
+    document.getElementById("second").style.transform = "rotate(" + secondAngle + "deg)";
+    document.getElementById("minute").style.transform = "rotate(" + minuteAngle + "deg)";
+    document.getElementById("hour").style.transform = "rotate(" + hourAngle + "deg)";
 }
 
 setInterval(time, 1000);
